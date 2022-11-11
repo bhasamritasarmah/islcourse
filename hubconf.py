de93230b865626d59ff2ed4a17a94e43536c09af
Bhasamrita Sarmah <68099728+cs21m004iittp@users.noreply.github.com>
2022-11-11 22:47:57 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -192,11 +192,49 @@ def _test_network(dataloader, model, loss_fn):
     return accuracy1,precision, recall, f1_score
   
 ##############################
+  
+def _test_network_advanced(dataloader, config, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            #X, y = X.to(device), y.to(device)
+            tmp = torch.nn.functional.one_hot(y, num_classes= 10)
+            pred = model(X, config)
+            test_loss += loss_fn(pred, tmp).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}")
+    #precision_recall_fscore_support(y_ground, y_pred, average='macro')
+    accuracy1 = Accuracy()
+    print('Accuracy :', accuracy1(pred,y))
+    precision = Precision(average = 'macro', num_classes = 10)
+    print('precision :', precision(pred,y))
+
+    recall = Recall(average = 'macro', num_classes = 10)
+    print('recall :', recall(pred,y))
+    f1_score = F1Score(average = 'macro', num_classes = 10)
+    print('f1_score :', f1_score(pred,y), '\n')
+    return accuracy1,precision, recall, f1_score
+  
+##############################
 
 def test_model(dataloader, model, loss_fn, epochs = 10):
   for e in range(epochs):
     print(f"Epochs {e+1}: ")
     _test_network(dataloader, model, loss_fn)
+
+  print("Finished Testing.")
+  
+##############################
+
+def test_model_advanced(dataloader, config, model, loss_fn, epochs = 10):
+  for e in range(epochs):
+    print(f"Epochs {e+1}: ")
+    _test_network_advanced(dataloader, config, model, loss_fn)
 
   print("Finished Testing.")
   
