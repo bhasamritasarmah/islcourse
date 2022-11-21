e3e34cc88699ebaadef4e13c441f64bcffd3775f
Bhasamrita Sarmah <68099728+cs21m004iittp@users.noreply.github.com>
2022-11-21 11:59:48 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,18 +1,21 @@
+import numpy as np
 from sklearn import datasets
 from sklearn.datasets import make_blobs
 from sklearn.datasets import make_circles
 from sklearn.cluster import KMeans
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import homogeneity_score, completeness_score, v_measure_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import make_scorer
 
 ##############################
-#Part 1
+#Part 1:
 
 def get_data_blobs(n_points = 100):
   x, y = make_blobs(n_samples = n_points, cluster_std = 0.2, random_state = 42)
@@ -38,7 +41,7 @@ def compare_clusterings(ypred_1 = None, ypred_2 = None):
   return homo, comp, vscore
 
 ##############################
-#Part 2
+#Part 2A:
 
 def get_data_mnist():
   data = datasets.load_digits()
@@ -66,4 +69,33 @@ def get_metrics(model1 = None, X = None, y = None):
   auc_ = auc(fpr, tpr)
   return acc, prec, rec, f1, auc_
 
+##############################
+#Part 2B:
+  
+def get_paramgrid_lr():
+  lr_param_grid = {
+      "C": np.logspace(-3, 3, 30), 
+      "penalty": ["l1", "l2"]
+      }
+  return lr_param_grid
+
+def get_paramgrid_rf():
+  rf_param_grid = { 
+    'n_estimators': [200, 500],
+    'max_features': ['auto', 'sqrt', 'log2'],
+    'max_depth' : [4,5,6,7,8],
+    'criterion' :['gini', 'entropy']
+    }
+  return rf_param_grid
+
+def perform_gridsearch_cv_multimetric(model1 = None, param_grid = None, cv = 5, X = None, y = None, metrics = ['accuracy','roc_auc']):
+  grid_search_cv = GridSearchCV(model1, param_grid, cv = cv)
+  grid_search_cv.fit(X, y)
+  
+  top1_scores = []
+  for metric in metrics:
+    score = make_scorer(metric)
+    top1_scores.append(score(model1, X, y))
+  
+  return top1_scores
 ##############################
