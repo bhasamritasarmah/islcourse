ddff5ac4284858e2e9c71b8e8e1dd3e25cde0b27
Bhasamrita Sarmah <68099728+cs21m004iittp@users.noreply.github.com>
2022-11-21 10:55:23 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,15 @@
+from sklearn import datasets
 from sklearn.datasets import make_blobs
 from sklearn.datasets import make_circles
 from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import homogeneity_score, completeness_score, v_measure_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_curve, auc
 
 ##############################
 #Part 1
@@ -28,5 +36,34 @@ def compare_clusterings(ypred_1 = None, ypred_2 = None):
   comp = completeness_score(ypred_1, ypred_2)
   vscore = v_measure_score(ypred_1, ypred_2)
   return homo, comp, vscore
+
+##############################
+#Part 2
+
+def get_data_mnist():
+  data = datasets.load_digits()
+  x = data.data
+  y = data.target
+  return x, y
+
+def build_lr_model(X = None, y = None):
+  lr = LogisticRegression(random_state = 42)
+  lr_model = lr.fit(X, y)
+  return lr_model
+
+def build_rf_model(X=None, y=None):
+  rf = RandomForestClassifier(max_depth = 3, random_state = 42)
+  rf_model = rf.fit(X, y)
+  return rf_model
+
+def get_metrics(model = None, X = None, y = None):
+  y_pred = model.predict(X)
+  acc = accuracy_score(y, y_pred)
+  prec = precision_score(y, y_pred, average = "weighted")
+  rec = recall_score(y, y_pred, average = "weighted")
+  f1 = f1_score(y, y_pred, average = "weighted")
+  tpr, fpr, thres = roc_curve(y, y_pred, pos_label = 2)
+  auc_ = auc(fpr, tpr)
+  return acc, prec, rec, f1, auc_
 
 ##############################
